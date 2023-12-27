e7afb1235f674426e6d00b0b34ecacd32f05a893
VenkataVijayaBhaskar007 <venkatavijayabhaskarvadada007@gmail.com>
2023-12-27 18:45:58 +0530

    added menu data

--- components/Header/menuData.tsx
+++ components/Header/menuData.tsx
@@ -5,7 +5,7 @@ const menuData: Menu[] = [
     id: 1,
     title: "Home",
     newTab: false,
-    path: "#home",
+    path: "/",
   },
   {
     id: 2,
@@ -13,8 +13,18 @@ const menuData: Menu[] = [
     newTab: false,
     path: "#about",
   },
-
-
+  {
+    id: 2,
+    title: "Services",
+    newTab: false,
+    path: "#services",
+  },
+  {
+    id: 2.3,
+    title: "Team",
+    newTab: false,
+    path: "/team",
+  },
   {
     id: 3,
     title: "Companies",
@@ -22,29 +32,34 @@ const menuData: Menu[] = [
     submenu: [
       {
         id: 31,
-        title: "Atoms digital solutins",
+        title: "Atoms Group",
         newTab: false,
-        path: "/atom",
+        path: "/",
       },
       {
         id: 34,
         title: "Atoms Education",
         newTab: false,
-        path: "/blog",
+        path: "https://atoms-education-omega.vercel.app/",
       },
       {
         id: 35,
-        title: "Atoms 2.0",
+        title: "Atoms Digital Solutins ",
         newTab: false,
-        path: "/atoms",
+        path: "https://digital-solutions-orpin.vercel.app/",
       },
-     
       {
-        id: 35.1,
-        title: "",
+        id: 35,
+        title: "Atoms 2.O",
         newTab: false,
-        path: "/support",
+        path: "http://atoms2-0.vercel.app",
       },
+      // {
+      //   id: 35.1,
+      //   title: "Support",
+      //   newTab: false,
+      //   path: "/support",
+      // },
       {
         id: 36,
         title: "",
@@ -53,12 +68,7 @@ const menuData: Menu[] = [
       },
     ],
   },
-  {
-    id: 2.3,
-    title: "Team",
-    newTab: false,
-    path: "/team",
-  },
+ 
 ];
 
 export default menuData;
