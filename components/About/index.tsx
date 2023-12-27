288222745e919fcf6d6d676c03d8b4ae3e0d6f74
VenkataVijayaBhaskar007 <venkatavijayabhaskarvadada007@gmail.com>
2023-12-28 01:33:25 +0530

    logo webp

--- components/About/index.tsx
+++ components/About/index.tsx
@@ -915,164 +915,164 @@ const About : React.FC = () => {
               
        
        
-           <img src="./images/clients/1.png" alt="client-1 logo image" />
+           <img src="./images/clients/1.webp" alt="client-1 logo image" />
        
        
        
-           <img src="./images/clients/2.png" alt="client-2 logo image" />
+           <img src="./images/clients/2.webp" alt="client-2 logo image" />
        
        
        
-           <img src="./images/clients/3.png" alt="client-3 logo image" />
+           <img src="./images/clients/3.webp" alt="client-3 logo image" />
        
        
        
-           <img src="./images/clients/4.png" alt="client-4 logo image" />
+           <img src="./images/clients/4.webp" alt="client-4 logo image" />
        
        
        
-           <img src="./images/clients/5.png" alt="client-5 logo image" />
+           <img src="./images/clients/5.webp" alt="client-5 logo image" />
        
        
        
-           <img src="./images/clients/6.png" alt="client-6 logo image" />
+           <img src="./images/clients/6.webp" alt="client-6 logo image" />
        
        
        
-           <img src="./images/clients/7.png" alt="client-7 logo image" />
+           <img src="./images/clients/7.webp" alt="client-7 logo image" />
        
        
        
-           <img src="./images/clients/8.png" alt="client-8 logo image" />
+           <img src="./images/clients/8.webp" alt="client-8 logo image" />
        
        
        
-           <img src="./images/clients/9.png" alt="client-9 logo image" />
+           <img src="./images/clients/9.webp" alt="client-9 logo image" />
        
        
        
        
-           <img src="./images/clients/10.png" alt="client-10 logo image" />
+           <img src="./images/clients/10.webp" alt="client-10 logo image" />
        
        
        
        
-           <img src="./images/clients/11.png" alt="client-11 logo image" />
+           <img src="./images/clients/11.webp" alt="client-11 logo image" />
        
        
        
        
-           <img src="./images/clients/12.png" alt="client-12 logo image" />
+           <img src="./images/clients/12.webp" alt="client-12 logo image" />
        
        
        
        
-           <img src="./images/clients/13.png" alt="client-13 logo image" />
+           <img src="./images/clients/13.webp" alt="client-13 logo image" />
        
        
        
        
-           <img src="./images/clients/14.png" alt="client-14 logo image" />
+           <img src="./images/clients/14.webp" alt="client-14 logo image" />
        
        
        
        
-           <img src="./images/clients/15.png" alt="client-15 logo image" />
+           <img src="./images/clients/15.webp" alt="client-15 logo image" />
        
        
        
        
-           <img src="./images/clients/16.png" alt="client-16 logo image" />
+           <img src="./images/clients/16.webp" alt="client-16 logo image" />
        
        
        
        
-           <img src="./images/clients/17.png" alt="client-17 logo image" />
+           <img src="./images/clients/17.webp" alt="client-17 logo image" />
        
        
        
        
-           <img src="./images/clients/18.png" alt="client-18 logo image" />
+           <img src="./images/clients/18.webp" alt="client-18 logo image" />
        
        
        
        
-           <img src="./images/clients/19.png" alt="client-19 logo image" />
+           <img src="./images/clients/19.webp" alt="client-19 logo image" />
        
        
        
        
-           <img src="./images/clients/20.png" alt="client-20 logo image" />
+           <img src="./images/clients/20.webp" alt="client-20 logo image" />
        
        
        
        
-           <img src="./images/clients/21.png" alt="client-21 logo image" />
+           <img src="./images/clients/21.webp" alt="client-21 logo image" />
        
        
        
        
-           <img src="./images/clients/22.png" alt="client-22 logo image" />
+           <img src="./images/clients/22.webp" alt="client-22 logo image" />
        
        
        
        
-           <img src="./images/clients/23.png" alt="client-23 logo image" />
+           <img src="./images/clients/23.webp" alt="client-23 logo image" />
        
        
        
        
-           <img src="./images/clients/24.png" alt="client-24 logo image" />
+           <img src="./images/clients/24.webp" alt="client-24 logo image" />
        
        
        
        
-           <img src="./images/clients/25.png" alt="client-25 logo image" />
+           <img src="./images/clients/25.webp" alt="client-25 logo image" />
        
        
        
        
-           <img src="./images/clients/26.png" alt="client-26 logo image" />
+           <img src="./images/clients/26.webp" alt="client-26 logo image" />
        
        
        
        
-           <img src="./images/clients/27.png" alt="client-27 logo image" />
+           <img src="./images/clients/27.webp" alt="client-27 logo image" />
        
        
        
        
-           <img src="./images/clients/28.png" alt="client-28 logo image" />
+           <img src="./images/clients/28.webp" alt="client-28 logo image" />
        
        
        
        
-           <img src="./images/clients/29.png" alt="client-29 logo image" />
+           <img src="./images/clients/29.webp" alt="client-29 logo image" />
        
        
        
        
-           <img src="./images/clients/30.png" alt="client-30 logo image" />
+           <img src="./images/clients/30.webp" alt="client-30 logo image" />
        
        
        
        
-           <img src="./images/clients/31.png" alt="client-31 logo image" />
+           <img src="./images/clients/31.webp" alt="client-31 logo image" />
        
        
        
        
-           <img src="./images/clients/32.png" alt="client-32 logo image" />
+           <img src="./images/clients/32.webp" alt="client-32 logo image" />
        
        
        
        
-           <img src="./images/clients/33.png" alt="client-33 logo image" />
+           <img src="./images/clients/33.webp" alt="client-33 logo image" />
        
        
        
        
-           <img src="./images/clients/34.png" alt="client-34 logo image" />
+           <img src="./images/clients/34.webp" alt="client-34 logo image" />
        
        
        
