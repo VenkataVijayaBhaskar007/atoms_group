18b23b475013765dd1d3b7266b4090a503eca9fe
VenkataVijayaBhaskar007 <venkatavijayabhaskarvadada007@gmail.com>
2023-12-28 20:01:04 +0530

    corrected the alignments

--- app/(site)/aboutus/page.tsx
+++ app/(site)/aboutus/page.tsx
@@ -13,7 +13,7 @@ const AboutUs = () => {
       <section className="overflow-hidden">
         <div className="bg-white relative pt-40 pb-20 lg:pt-44 dark:bg-gray-900">
           <div className="relative xl:container m-auto px-6 md:px-12 lg:px-6">
-            <h1 className="sm:mx-auto sm:w-10/12 md:w-2/3 font-black text-blue-900 text-2xl text-center sm:text-5xl md:text-4xl lg:w-auto lg:text-left xl:text-6xl   dark:text-white">Transforming vision into <br className="lg:block hidden"/><span className="relative text-transparent bg-clip-text bg-gradient-to-r from-blue-600 to-cyan-500 dark:from-blue-400 dark:to-cyan-300">reality.</span>.</h1>
+            <h1 className="sm:mx-auto sm:w-10/12 md:w-2/3 font-black text-blue-900 text-2xl text-center sm:text-5xl md:text-4xl lg:w-auto lg:text-left xl:text-5xl   dark:text-white">Transforming vision into <br className="lg:block hidden"/><span className="relative text-transparent bg-clip-text bg-gradient-to-r from-blue-600 to-cyan-500 dark:from-blue-400 dark:to-cyan-300">Reality.</span>.</h1>
             <div className="lg:flex">
               <div className="relative mt-8 md:mt-16 space-y-8 sm:w-10/12 md:w-2/3 lg:ml-0 sm:mx-auto text-center lg:text-left lg:mr-auto lg:w-7/12">
                 <p className="sm:text-lg text-gray-700 dark:text-gray-300 lg:w-11/12">
