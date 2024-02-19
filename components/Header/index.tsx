87504598ca4c787ce4a3302e2ec5bb80e304c1c3
VenkataVijayaBhaskar007 <venkatavijayabhaskarvadada007@gmail.com>
2024-02-19 12:10:03 +0530

    aa

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -165,7 +165,7 @@ const Header = () => {
               href="tel:8688275808"
               className="flex items-center justify-center rounded-full bg-primary px-5 py-2.5 text-regular text-white duration-300 ease-in-out hover:bg-blue-900 hover:translate-y-[-8px]"
             >
-              contact US 
+              Contact US 
             </Link>
           </div>
         </div>
