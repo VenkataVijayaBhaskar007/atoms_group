5b301c88805473f12ef05c3f80d526b565967fa3
VenkataVijayaBhaskar007 <venkatavijayabhaskarvadada007@gmail.com>
2024-02-21 17:04:04 +0530

    hello

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -37,7 +37,9 @@ const Header = () => {
     if (isMobileDevice()) {
       // If it's a mobile device, set the link for a direct call
       contactLink.href = "tel:8688275808";
-    } else {
+    } 
+    
+    else {
       // If it's not a mobile device, set the link for sending an email
       contactLink.href = "mailto:atomsgroup01@gmail.com";
     }
@@ -173,7 +175,7 @@ const Header = () => {
           </nav>
 
           <div id="contactSection" className="mt-7 flex items-center gap-6 xl:mt-0">
-  <a id="contactLink" href="#" className="flex items-center justify-center rounded-full bg-primary px-5 py-2.5 text-regular text-white duration-300 ease-in-out hover:bg-blue-900 hover:translate-y-[-8px]">
+  <a id="contactLink"  className="flex items-center justify-center rounded-full bg-primary px-5 py-2.5 text-regular text-white duration-300 ease-in-out hover:bg-blue-900 hover:translate-y-[-8px]">
     Contact Us
   </a>
 </div>
